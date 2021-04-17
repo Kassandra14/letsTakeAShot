fde3cd389137798f777a6b9129383aeba1003a11
@kcruit-salzberg <kassandra.cruit-salzberg@alsoenergy.com>
2021-04-17 08:47:21 -0600

    grammer error fix

--- map.js
+++ map.js
@@ -1,5 +1,8 @@
 var mymap = L.map('mapid').setView([39.867376020395135, -104.67181074323783], 10);
 
+// var popup = L.popup();
+// var denver    = L.marker([39.867376020395135, -104.67181074323783]).bindPopup('This is Denver Airport!.');
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -7,9 +10,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoia2FzMTQiLCJhIjoiY2tuYzB4NHg1MHpkNjJwazg3NHE1Z2Y2MSJ9.0hf0lV2dKaQd2V-lFxdJ4g'
+    
 }).addTo(mymap);
 
-map.locate({watch: true})
+mymap.locate({watch: true})
 
 
 var popup = L.popup();
@@ -23,16 +27,5 @@ function onMapClick(e) {
 
 mymap.on('click', onMapClick);
 
-// var map = L.map('map', {
-//     minZoom: 10,
-//     maxZoom: 10
-// });
 
-// var cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
-
-// var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-//     attribution: cartodbAttribution
-// }).addTo(map);
-
-// map.setView([41.00058,-106.30034], 0);
 
